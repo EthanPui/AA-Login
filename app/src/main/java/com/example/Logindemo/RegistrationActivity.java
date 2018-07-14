56d6726a8d25e5fd75ed6fc0fe3ffc51af058070
Ethan Pui <ethanpui@gmail.com>
2018-07-14 17:13:05 +0800

    Part 7
    No Errors

--- app/src/main/java/com/example/Logindemo/RegistrationActivity.java
+++ app/src/main/java/com/example/Logindemo/RegistrationActivity.java
@@ -1,12 +1,14 @@
 package com.example.Logindemo;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,13 +18,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText userName, userPassword, userEmail;
+    private EditText userName, userPassword, userEmail, userAge;
     private Button regButton;
     private TextView userLogin;
     private FirebaseAuth firebaseAuth;
+    private ImageView userProfilePic;
+    String email, name, age, password;
 
 
     @Override
@@ -46,7 +52,11 @@ public class RegistrationActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
                             if (task.isSuccessful()){
-                                sendEmailVerificationl();
+                                //sendEmailVerificationl();
+                                sendUserData();
+                                Toast.makeText(RegistrationActivity.this, "Successfully Registered, Upload complete", Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
                             }else{
                                 Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
                             }
@@ -74,18 +84,19 @@ public class RegistrationActivity extends AppCompatActivity {
         userEmail = (EditText) findViewById(R.id.etUserEmail);
         regButton = (Button)findViewById(R.id.btnRegister);
         userLogin = (TextView)findViewById(R.id.tvUserLogin);
-
-
+        userAge = (EditText)findViewById(R.id.etAge);
+        userProfilePic = (ImageView)findViewById(R.id.ivProfile);
     }
 
     private Boolean validate(){
         Boolean result = false;
 
-        String name = userName.getText().toString();
-        String password = userPassword.getText().toString();
-        String email = userEmail.getText().toString();
+         name = userName.getText().toString();
+         password = userPassword.getText().toString();
+         email = userEmail.getText().toString();
+         age = userAge.getText().toString();
 
-        if (name.isEmpty() || password.isEmpty() || email.isEmpty()){
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty() || age.isEmpty()){
             Toast.makeText(this,"Please enter all the details", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
 
@@ -106,6 +117,7 @@ public class RegistrationActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
                     if (task.isSuccessful()){
+                        sendUserData();
                         Toast.makeText(RegistrationActivity.this, "Successfully Registered, Verification mail sent!", Toast.LENGTH_SHORT).show();
                         firebaseAuth.signOut();
                         finish();
@@ -117,6 +129,13 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void sendUserData(){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = firebaseDatabase.getReference(firebaseAuth.getUid());
+        UserProfile userProfile = new UserProfile(age, email, name);
+        myRef.setValue(userProfile);
     }
 
 
